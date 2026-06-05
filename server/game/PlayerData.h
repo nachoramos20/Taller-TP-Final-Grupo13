2494c0bf18490df1d95fb204f0647419c3a621a3
nachoramos20 <ramosnacho3237@gmail.com>
2026-06-05 00:19:15 -0300

    feat: actualizo PlayerData

--- server/game/PlayerData.h
+++ server/game/PlayerData.h
@@ -3,24 +3,42 @@
 
 #include <string>
 #include <cstdint>
+#include <array>
+#include "../../common/protocol/protocol.h"
 
 struct PlayerData {
     std::string username;
-    uint16_t entity_id;
-    uint8_t  race;
-    uint8_t  cls;
-    uint16_t pos_x;
-    uint16_t pos_y;
-    uint8_t  direction;
-    uint16_t hp;
-    uint16_t max_hp;
-    uint16_t mp;
-    uint16_t max_mp;
-    uint32_t exp;
-    uint8_t  level;
-    uint32_t gold;
-    bool     is_ghost;
+    uint16_t entity_id   = 0;
+    uint8_t  race        = 0;
+    uint8_t  cls         = 0;
+    uint16_t pos_x       = 0;
+    uint16_t pos_y       = 0;
+    uint8_t  direction   = 0;
 
+    uint16_t hp          = 0;
+    uint16_t max_hp      = 0;
+    uint16_t mp          = 0;
+    uint16_t max_mp      = 0;
+    uint32_t exp         = 0;
+    uint8_t  level       = 1;
+    uint32_t gold        = 0;
+    bool     is_ghost    = false;
+    bool     meditating  = false;
+
+    uint16_t strength    = 18;
+    uint16_t agility     = 18;
+    uint16_t intelligence= 18;
+    uint16_t constitution= 18;
+
+    static constexpr int INVENTORY_SIZE = 16;
+    std::array<uint8_t, INVENTORY_SIZE> inventory{};
+
+    uint8_t equipped_weapon = 0;
+    uint8_t equipped_armor  = 0;
+    uint8_t equipped_helmet = 0;
+    uint8_t equipped_shield = 0;
+
+    uint16_t attack_cooldown = 0;
 };
 
-#endif // PLAYERDATA_H
+#endif
